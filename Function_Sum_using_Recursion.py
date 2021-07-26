effb4da6a6fa3a01e77e8c1d89620236f9146a40
Kanhaiyak424 <Kanhaiyak424@gmail.com>
2021-07-26 11:43:39 +0000

    Sum function using Recursion

---
+++ Function_Sum_using_Recursion.py
@@ -0,0 +1,9 @@
+#Program for summation of natural number using Recurrsion
+
+n=int(input("Enter a number:"));
+def sum(a):
+ if a<=1:
+  return 1;
+ else:
+  return a+sum(a-1);
+print("The summation of given series is",sum(n));
